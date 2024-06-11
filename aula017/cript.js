bd2df444aecf721be4b3be01d740c9cda41ce6e6
Rod0098 <rodrigo8.00.rd@gmail.com>
2024-06-10 23:27:03 -0300

    novo exercicio

--- aula017/cript.js
+++ aula017/cript.js
@@ -5,15 +5,27 @@ function contar() {
    var res = window.document.getElementById('res')
 
    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) { 
+      res.innerHTML = `Impossivel contar`
       window.alert('[ERRO] Faltam dados!' )
    } else {
-      res.innerHTML = 'Contando:'
+      res.innerHTML = 'Contando: '
       var i = Number(ini.value)
       var f = Number(fim.value)
       var p = Number(pas.value)
-  
+      if (p <= 0) {
+         window.alert('Passo inválido! Considerando PASSO 1')
+         p = 1
+      }
+      if (i < f) {
       for(var c = i;  c <= f; c += p) {
-         res.innerHTML += `${c}`
-      } 
+            res.innerHTML += ` ${c} \u{1F449} `
+         } 
+         
+      } else {
+         for(var c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+         }
+      }
+      res.innerHTML += `\u{1F3C1}`
    } 
 }
