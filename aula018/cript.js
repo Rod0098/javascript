355d8caf6b0ed51480b1afb08cee3b54ce67bbc8
Rod0098 <rodrigo8.00.rd@gmail.com>
2024-06-12 10:02:39 -0300

    novo exercício com javascript

--- aula018/cript.js
+++ aula018/cript.js
@@ -6,8 +6,12 @@ function tabuada() {
 } else {
    var n = Number(num.value)
    var c = 1
+   tab.innerHTML = ''
    while (c <= 10) {
-     var item = document
+     var item = document.createElement('option')
+     item.text =`${n} x ${c} = ${n*c}`
+     tab.appendChild(item)
+     c++
    }
  }
 }
